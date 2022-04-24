4f565b674146700ea65f8926257f6bba2b442b0c
zhangrmatthew <56270011+zhangrmatthew@users.noreply.github.com>
2022-04-24 18:37:55 -0400

    TDD Book, Chapter 5

--- functional_tests.py
+++ functional_tests.py
@@ -9,6 +9,11 @@ class NewVisitorTest(unittest.TestCase):
 
     def tearDown(self):
         self.browser.quit()
+
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id("id_list_table")
+        rows = self.browser.find_elements_by_tag_name("tr")
+        self.assertIn(row_text, [row.text for row in rows])
     
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
@@ -35,17 +40,25 @@ class NewVisitorTest(unittest.TestCase):
         # "1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
-        table = self.browser.find_element_by_id("id_list_table")
-        rows = self.browser.find_elements_by_tag_name("tr")
-        self.assertTrue(
-            any(row.text == "1: Buy peakcock feathers" for row in rows)
-        )
+        
+        # self.assertTrue(
+        #     any(row.text == "1: Buy peakcock feathers" for row in rows),
+        #     #Custom error message to most assertX methods in unitttest
+        #     f"New todo item did not appear in table. Contents were :\n{table.text}"
+        # )
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
+        inputbox = self.browser.find_element_by_id("id_new_item")
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
         # The page updates again, and now shows both items on her list
+        self.check_for_row_in_list_table("1: Buy peacock feathers")
+        self.check_for_row_in_list_table("2: Use peacock feathers to make a fly")
 
         # Edith wonders whether the site will remember her list. Then she sees
         # that the site has generated a unique URL for her -- there is some
--- lists/views.py
+++ lists/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
-from django.shortcuts import render
+from lists.models import Item
 
 # Create your views here.
 def home_page(request):
-    return render(request, "home.html")
+    # if request.method == "POST":
+    #     # request.POST['item_text'] gives us the request value associated with the key
+    #     return HttpResponse(request.POST['item_text'])
+    if request.method == "POST":
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/')
+    # else:
+    #     new_item_text = ' '
+    # item = Item()
+    # item.text =   request.POST.get('item_text', " ")
+    # item.save()
+    items = Item.objects.all()
+    return render(request, "home.html", {'items': items})
